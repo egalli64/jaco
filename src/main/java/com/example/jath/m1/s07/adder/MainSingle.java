f3adcfac472746d44a72bc359056438a4d315232
Emanuele <egalli64@gmail.com>
2022-03-16 17:33:09 +0100

    minor cleanup

--- src/main/java/com/example/jath/m1/s07/adder/MainSingle.java
+++ src/main/java/com/example/jath/m1/s07/adder/MainSingle.java
@@ -2,6 +2,7 @@ package com.example.jath.m1.s07.adder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.LongStream;
 
@@ -10,7 +11,7 @@ public class MainSingle {
     private static final int NR = 10;
 
     public static void main(String[] args) {
-        TreeSet<Double> results = new TreeSet<>();
+        Set<Double> results = new TreeSet<>();
         List<Long> times = new ArrayList<>();
         long[] data = LongStream.rangeClosed(1, SIZE).toArray();
         System.out.println("Values in data: " + data[0] + " ... " + data[data.length - 1]);
@@ -24,7 +25,7 @@ public class MainSingle {
         if (results.size() != 1) {
             System.out.println("Unexpected number of results!");
         } else {
-            System.out.println("Result is " + results.first());
+            System.out.printf("Result is %f%n", results.iterator().next());
         }
 
         if (results.isEmpty()) {
@@ -35,7 +36,7 @@ public class MainSingle {
     }
 
     private static double plainCubeRootAdder(long[] data) {
-        double result = 0;
+        double result = 0.0;
         for (long value : data) {
             result += Math.cbrt(value);
         }
