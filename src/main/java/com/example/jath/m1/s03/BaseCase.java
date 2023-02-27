ec6079a42a0552e402fe56cb06feb18fd615a5c6
Emanuele <egalli64@gmail.com>
2023-02-27 11:51:59 +0100

    more comments

--- src/main/java/com/example/jath/m1/s03/BaseCase.java
+++ src/main/java/com/example/jath/m1/s03/BaseCase.java
@@ -1,20 +1,38 @@
+/*
+ * Introduction to Java Thread
+ * 
+ * https://github.com/egalli64/jath
+ */
 package com.example.jath.m1.s03;
 
 import java.lang.Thread.State;
 
 /**
- * Enable assertions with -ea argument
+ * Thread states
+ * 
+ * Enable assertions with -ea VM argument
  */
 public class BaseCase {
-    public static void main(String[] args) throws InterruptedException {
+    /**
+     * Create a simple thread and let it run
+     * 
+     * @param args not used
+     */
+    public static void main(String[] args) {
         String name = "base";
         System.out.printf("Thread states for %s%n", name);
+
+        // Create a thread with a given name, the passed runnable is its task
         Thread t = new Thread(() -> System.out.printf("Hello from %s%n", name), name);
 
+        // The thread is in its initial state
         assert t.getState() == State.NEW && !t.isAlive();
         System.out.printf("Thread %s state is %s%n", t.getName(), t.getState());
 
+        // Starting the thread, transition from NEW to RUNNABLE state
         t.start();
+
+        // The process now has two alive threads, main and base
         assert t.getState() == State.RUNNABLE || t.getState() == State.TERMINATED;
         System.out.printf("Thread %s state is %s%n", t.getName(), t.getState());
         if (t.isAlive()) {
@@ -23,7 +41,10 @@ public class BaseCase {
             System.out.printf("If main thread is slow, the other thread could terminate in the meantime!");
         }
 
-        Thread.sleep(100);
+        // Some other task executed by the main thread
+        anotherTask();
+
+        // Now we could reasonably expect the base thread to have reached its end
         assert t.getState() == State.TERMINATED && !t.isAlive();
         System.out.printf("Thread %s state is %s%n", t.getName(), t.getState());
         if (!t.isAlive()) {
@@ -31,5 +52,17 @@ public class BaseCase {
         }
 
         System.out.println("Bye");
+    }
+
+    /**
+     * Task simulation, just waste some run time
+     */
+    private static void anotherTask() {
+        try {
+            // This is just a simulation! The production use of sleep() is very limited!
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
--- src/main/java/com/example/jath/m1/s03/Waiting.java
+++ src/main/java/com/example/jath/m1/s03/Waiting.java
@@ -1,26 +1,65 @@
+/*
+ * Introduction to Java Thread
+ * 
+ * https://github.com/egalli64/jath
+ */
 package com.example.jath.m1.s03;
 
 import java.lang.Thread.State;
 
 /**
- * Enable assertion with -ea argument
+ * Show waiting and time waiting threads
+ * 
+ * Enable assertion with -ea VM argument
  */
 public class Waiting {
+    /**
+     * Create a thread that goes immediately in timed waiting state. Put the main thread in wait for the
+     * end of the child thread, then terminate.
+     * 
+     * @param args not used
+     * @throws InterruptedException if wait is interrupted
+     */
+    public static void main(String[] args) throws InterruptedException {
+        // Create and start the thread "waited", that runs the specified method
+        Thread t1 = new Thread(Waiting::aMethod, "waited");
+        t1.start();
+
+        // Do something else on main thread, so we can safely assume t1 kicks in
+        anotherJob(50);
+
+        // Now t1 is expected to be in a timed waiting
+        assert t1.getState() == State.TIMED_WAITING;
+        System.out.printf("Checking %s thread from main, its state is %s%n", t1.getName(), t1.getState());
+
+        // Put the main thread in waiting for t1 to terminate
+        t1.join();
+
+        System.out.println("Bye");
+    }
+
+    /**
+     * A method meant to run in its own thread.
+     * 
+     * It simulates a timed waiting on a resource, then check that main is waiting for its termination.
+     */
     public static void aMethod() {
-        System.out.printf("Emulating a long job for %s thread%n", Thread.currentThread().getName());
+        System.out.printf("Simulating a wait on a resource for %s thread%n", Thread.currentThread().getName());
         try {
+            // This is just a simulation! The production use of sleep() is very limited!
             Thread.sleep(500);
 
-            System.out.print("Checking main status from " + Thread.currentThread().getName());
+            System.out.printf("Checking from '%s' thread: ", Thread.currentThread().getName());
             Thread[] ts = new Thread[2];
-            // Thread::enumerate() should be used only for debugging and monitoring purposes
+            // Thread::enumerate() should be used for debugging and monitoring purposes only
             int count = Thread.enumerate(ts);
             for (int i = 0; i < count; i++) {
                 if (ts[i].getName().equals("main")) {
                     State state = ts[i].getState();
 
+                    // the main thread state should be waiting - being in join on this thread
                     assert state == State.WAITING;
-                    System.out.printf(", its state is %s%n", state);
+                    System.out.println("main state is " + state);
                     break;
                 }
             }
@@ -29,18 +68,17 @@ public class Waiting {
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new Thread(Waiting::aMethod, "waited");
-        t1.start();
-
-        System.out.printf("Emulating a short job, to let %s thread to kick in%n", t1.getName());
-        Thread.sleep(50);
-
-        System.out.printf("Checking %s from main,", t1.getName());
-        System.out.printf(" its state is %s%n", t1.getState());
-        assert t1.getState() == State.TIMED_WAITING;
-
-        t1.join();
-        System.out.println("Bye");
+    /**
+     * It simulates to be busy in doing something for a while.
+     * 
+     * @param millis time to be busy, in milliseconds
+     */
+    private static void anotherJob(long millis) {
+        try {
+            System.out.printf("Do something in %s thread for %dms%n", Thread.currentThread().getName(), millis);
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
