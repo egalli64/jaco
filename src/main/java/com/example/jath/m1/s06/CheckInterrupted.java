93329e10974e37edaa465d3d11f778f4b45092b5
Emanuele <egalli64@gmail.com>
2023-02-27 16:38:45 +0100

    improve readability

--- src/main/java/com/example/jath/m1/s06/CheckInterrupted.java
+++ src/main/java/com/example/jath/m1/s06/CheckInterrupted.java
@@ -1,10 +1,26 @@
+/*
+ * Introduction to Java Thread
+ * 
+ * https://github.com/egalli64/jath
+ */
 package com.example.jath.m1.s06;
 
+/**
+ * Working with two threads, one is going to interrupt the other one.
+ * 
+ * Notice that the console is carelessly shared between two threads. Expect troubles.
+ */
 public class CheckInterrupted {
+    /**
+     * Create another thread and start it. After a while interrupt it.
+     * 
+     * @param args not used
+     */
     public static void main(String[] args) {
         System.out.println("In thread " + Thread.currentThread().getName());
 
-        Thread other = new Thread(() -> {
+        // What is going to be executed by the other thread. Only an interrupt will stop it.
+        Runnable runnable = () -> {
             final Thread cur = Thread.currentThread();
             System.out.println("In thread " + cur.getName());
 
@@ -17,16 +33,17 @@ public class CheckInterrupted {
             }
 
             System.out.printf("Thread %s is done%n", cur.getName());
-        }, "other");
+        };
 
-        System.out.printf("The thread %s has not started yet%n", other.getName());
+        Thread other = new Thread(runnable, "other");
+
+        System.out.println("!!! Two threads are going to compete on System.out !!!");
         System.out.println("!!! Output could be garbled  !!!");
-        System.out.println("!!! Two threads are competing on System.out !!!");
 
         other.start();
 
         for (int i = 0; i < 100; i++) {
-            System.out.print("Simulating a job in main ... ");
+            System.out.printf("Simulating a job in %s ... ", Thread.currentThread().getName());
             System.out.println(i);
         }
 
--- src/main/java/com/example/jath/m1/s06/CheckInterruptedExceptional.java
+++ src/main/java/com/example/jath/m1/s06/CheckInterruptedExceptional.java
@@ -1,46 +1,70 @@
+/*
+ * Introduction to Java Thread
+ * 
+ * https://github.com/egalli64/jath
+ */
 package com.example.jath.m1.s06;
 
+/**
+ * What happens when an interrupt is sent to a waiting (here, sleeping) thread
+ */
 public class CheckInterruptedExceptional {
+    /**
+     * A thread is created and started. After a while an interrupt is sent to it. However, that thread
+     * could be waiting for something to happen (here, the wait is simulated by sleeping). So the
+     * interrupt could be managed in the "normal" way, or could require the catch of an
+     * InterruptedException.
+     * 
+     * @param args not used
+     */
     public static void main(String[] args) {
         System.out.println("In thread " + Thread.currentThread().getName());
 
-        Thread other = new Thread(() -> {
+        // What is going to be executed by the other thread. Only an interrupt will stop it.
+        Runnable runnable = () -> {
             final Thread cur = Thread.currentThread();
             System.out.println("In thread " + cur.getName());
 
             try {
-                int i = 0;
                 while (!cur.isInterrupted()) {
-                    System.out.print("Simulating a job in " + cur.getName() + " ... ");
-                    Thread.sleep(100);
-                    System.out.println(i++);
+                    int i = 1;
+                    System.out.print("Simulating that " + cur.getName() + " is waiting on a resource ... ");
+                    // This is just a simulation! The use of sleep() in production code is very limited!
+                    Thread.sleep(2);
+                    while (i % 100 != 0) {
+                        System.out.println(i++);
+                    }
                 }
+                // thread interrupted when runnable
                 System.out.printf("Someone has interrupted %s!%n", cur.getName());
             } catch (InterruptedException e) {
-                System.out.printf("Someone has interrupted %s in its sleep!%n", cur.getName());
+                // thread interrupted when waiting, sleeping, or otherwise occupied
+                System.out.printf("InterruptedException detected in %s!%n", cur.getName());
                 // reset the flag on the current thread as interrupted
                 cur.interrupt();
             }
 
+            // Thread has been interrupted, terminate it
             System.out.printf("Thread %s is done%n", cur.getName());
-        }, "other");
+        };
 
-        System.out.printf("The thread %s has not started yet%n", other.getName());
+        Thread other = new Thread(runnable, "other");
+
+        System.out.println("!!! Two threads are going to compete on System.out !!!");
         System.out.println("!!! Output could be garbled  !!!");
-        System.out.println("!!! Two threads are competing on System.out !!!");
 
         other.start();
 
         try {
             System.out.println("Simulating a job in main, let other thread having some fun ...");
-            Thread.sleep(500);
+            // This is just a simulation! The use of sleep() in production code is very limited!
+            Thread.sleep(5);
         } catch (InterruptedException e) {
             throw new IllegalStateException(e);
         }
 
         System.out.println("Thread main has enough of other thread!");
         other.interrupt();
-
-        System.out.printf("Thread %s is done%n", Thread.currentThread().getName());
+        System.out.println("Bye");
     }
 }
