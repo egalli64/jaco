81a760caefbc1a8c7c4e5363b15573ba74148ecb
Emanuele <egalli64@gmail.com>
2023-02-27 21:29:42 +0100

    more comments

--- src/main/java/com/example/jath/m1/s07/MainLongRunner.java
+++ src/main/java/com/example/jath/m1/s07/MainLongRunner.java
@@ -1,35 +1,47 @@
+/*
+ * Introduction to Java Thread
+ * 
+ * https://github.com/egalli64/jath
+ */
 package com.example.jath.m1.s07;
 
+/**
+ * Using Thread::isAlive() and Thread::join() with time
+ */
 public class MainLongRunner {
+    /**
+     * Create a thread, join with time limit. Maybe the other thread terminate before, maybe not.
+     */
     public static void main(String[] args) {
+        // Creating a worker with a time consuming job
         Thread t0 = new Thread(() -> {
-            System.out.println("A message from " + Thread.currentThread().getName());
-            try {
-                System.out.println("Simulating a long job for " + Thread.currentThread().getName());
-                Thread.sleep(1_000);
-            } catch (InterruptedException e) {
-                System.out.println(Thread.currentThread().getName() + " interrupted");
-                throw new IllegalStateException(e);
+            System.out.println("Starting a worker");
+
+            double result = 0.0;
+            for (int i = 1; i < 30_000_000; i++) {
+                result += Math.cbrt(Math.pow(i, Math.PI));
             }
-            System.out.printf("%s is done now", Thread.currentThread().getName());
+
+            System.out.printf("Result is %.0f%n", result);
         });
 
         t0.start();
 
         try {
             System.out.println("Waiting a while for the other thread, then go back to do other stuff");
-            t0.join(500);
+            t0.join(20);
         } catch (InterruptedException e) {
-            System.out.println(t0.getName() + " interrupted");
             throw new IllegalStateException(e);
         }
 
+        // Maybe the worker is still alive, maybe not
         if (t0.isAlive()) {
-            System.out.printf("After timed join(), but %s is still alive%n", t0.getName());
+            System.out.println("After timed join(), but the worker is still alive");
         } else {
-            System.out.println("You should not get this message");
+            System.out.println("The worker was so fast that it has already terminated!");
         }
 
-        System.out.println("Terminating main thread");
+        // In any case, terminate the main thread
+        System.out.println("Bye");
     }
 }
