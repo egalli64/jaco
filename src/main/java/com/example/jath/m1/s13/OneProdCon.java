3a86ff6074a800fb1ad9dcdd69aa00784a307820
Emanuele <egalli64@gmail.com>
2023-03-01 22:05:01 +0100

    more comments

--- src/main/java/com/example/jath/m1/s13/OneProdCon.java
+++ src/main/java/com/example/jath/m1/s13/OneProdCon.java
@@ -1,27 +1,48 @@
+/*
+ * Introduction to Java Thread
+ * 
+ * https://github.com/egalli64/jath
+ */
 package com.example.jath.m1.s13;
 
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * Lock and Condition
+ * 
+ * A simple exchange between a producer and a consumer
+ */
 public class OneProdCon {
-    private volatile double result;
+    private double result;
+    private boolean produced;
     private Lock lock;
     private Condition available;
 
+    /**
+     * Constructor
+     */
     public OneProdCon() {
         this.result = 0.0;
+        this.produced = false;
         this.lock = new ReentrantLock();
         this.available = lock.newCondition();
     }
 
+    /**
+     * For the producer thread.
+     * 
+     * Acquire the lock, produce the result, signal() on the condition its availability.
+     */
     private void producer() {
         String name = Thread.currentThread().getName();
         System.out.println(name + " enter producer()");
 
         try {
             lock.lock();
-            result = Math.cbrt(Math.random());
+            result = Math.random();
+            produced = true;
             System.out.printf("%s has produced as result %f%n", name, result);
             available.signal();
         } finally {
@@ -29,20 +50,25 @@ public class OneProdCon {
         }
     }
 
+    /**
+     * For the consumer thread.
+     * 
+     * Acquire the lock, if the product is not ready, await() on the condition for it. Then consume it.
+     */
     private void consumer() {
         String name = Thread.currentThread().getName();
         System.out.println(name + " enter consumer()");
 
         try {
             lock.lock();
-            while (result == 0.0) {
+            while (!produced) {
                 System.out.println(name + " waits for the result");
                 available.await();
                 System.out.println(name + " wait is ended");
             }
 
             System.out.printf("%s consumes %f%n", name, result);
-            result = 0.0;
+            produced = false;
         } catch (InterruptedException e) {
             throw new IllegalStateException(e);
         } finally {
@@ -50,22 +76,25 @@ public class OneProdCon {
         }
     }
 
-    public static void main(String[] args) {
+    /**
+     * Create and start a consumer thread, that it is going to hang, waiting for the producer. Then
+     * create and start a producer thread, that would signal its production to the consumer.
+     * 
+     * @param args not used
+     * @throws InterruptedException when interrupted
+     */
+    public static void main(String[] args) throws InterruptedException {
         OneProdCon wn = new OneProdCon();
 
-        Thread ts[] = { new Thread(wn::consumer, "TC"), new Thread(wn::producer, "TP") };
+        Thread consumer = new Thread(wn::consumer, "TC");
+        consumer.start();
 
-        for (Thread t : ts) {
-            t.start();
-        }
+        Thread producer = new Thread(wn::producer, "TP");
+        producer.start();
 
-        for (Thread t : ts) {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                throw new IllegalStateException(e);
-            }
-        }
+        System.out.println("Main thread waits end of exachange");
+        producer.join();
+        consumer.join();
 
         System.out.println("Bye");
     }
--- src/main/java/com/example/jath/m1/s13/OneProdManyCons.java
+++ src/main/java/com/example/jath/m1/s13/OneProdManyCons.java
@@ -1,3 +1,8 @@
+/*
+ * Introduction to Java Thread
+ * 
+ * https://github.com/egalli64/jath
+ */
 package com.example.jath.m1.s13;
 
 import java.util.concurrent.TimeUnit;
@@ -5,94 +10,104 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * Lock and Condition
+ * 
+ * A simple exchange between a producer and a few consumers
+ */
 public class OneProdManyCons {
-    private volatile double result;
+    private double product;
+    private boolean consumed;
     private Lock lock;
-    private Condition available;
-    private Condition consumed;
+    private Condition availability;
 
+    /**
+     * Constructor
+     */
     public OneProdManyCons() {
-        this.result = 0.0;
+        this.product = 0.0;
+        this.consumed = true;
         this.lock = new ReentrantLock();
-        this.available = lock.newCondition();
-        this.consumed = lock.newCondition();
+        this.availability = lock.newCondition();
     }
 
-    public void checkThreadStates() {
-        System.out.println("Checking thread states from " + Thread.currentThread().getName());
-        Thread[] ts = new Thread[6];
-        // Thread::enumerate() should be used only for debugging and monitoring purposes
-        int count = Thread.enumerate(ts);
-        for (int i = 0; i < count; i++) {
-            System.out.printf("%s is %s%n", ts[i].getName(), ts[i].getState());
-        }
-    }
-
+    /**
+     * For the producer thread, loop until interrupted.
+     * 
+     * Produce a product, signal it to all, then wait its consumption before produce again.
+     */
     private void producer() {
         try {
             lock.lock();
+            System.out.println("Producer has acquired the lock");
             while (!Thread.currentThread().isInterrupted()) {
-                result = Math.cbrt(Math.random());
-                System.out.println(result + " produced");
+                product = Math.random();
+                consumed = false;
 
-                checkThreadStates();
-                available.signalAll();
-                consumed.await();
-                while (result != 0.0) {
-                    System.out.println("Producer waits the result to be consumed");
-                    consumed.await(500, TimeUnit.MILLISECONDS);
+                System.out.printf("Produced %f and signal availability%n", product);
+                availability.signalAll();
+                while (!consumed) {
+                    System.out.println("Producer waits the product to be consumed");
+                    availability.await(500, TimeUnit.MILLISECONDS);
                     System.out.println("Producer wait is ended");
                 }
             }
+            System.out.println("Producer production has been interrupted");
         } catch (InterruptedException e) {
-            System.out.println("Producer has been interrupted");
+            System.out.println("Producer wait has been interrupted");
             Thread.currentThread().interrupt();
         } finally {
             lock.unlock();
-            System.out.println("Producer has stopped producing values");
+            System.out.println("Producer has relased the lock and is terminating");
         }
     }
 
+    /**
+     * For the consumer thread.
+     * 
+     * Wait until has access to a not-already-consumed product. Signal that, and then terminate.
+     */
     private void consumer() {
         String name = Thread.currentThread().getName();
         try {
             lock.lock();
-            while (result == 0.0) {
-                System.out.println(name + " waits for the result");
-                available.await();
+            System.out.println(name + " has acquired the lock");
+            while (consumed) {
+                System.out.println(name + " waits for a product not already consumed");
+                availability.await();
                 System.out.println(name + " wait is ended");
             }
 
-            System.out.printf("Consumer %s consumes %f%n", name, result);
-            result = 0.0;
-
-            checkThreadStates();
-            consumed.signal();
+            System.out.printf("%s signals consumation of %f%n", name, product);
+            consumed = true;
+            availability.signalAll();
         } catch (InterruptedException e) {
             throw new IllegalStateException(e);
         } finally {
             lock.unlock();
-            System.out.println(name + " exits consumer()");
+            System.out.println(name + " has released the lock and is terminating");
         }
     }
 
+    /**
+     * Create and start the producer, then create the consumers.
+     * 
+     * Join on them, and then terminate the producer.
+     * 
+     * @param args not used
+     * @throws InterruptedException when interrupted
+     */
     public static void main(String[] args) throws InterruptedException {
         OneProdManyCons wn = new OneProdManyCons();
 
-        Thread producer = new Thread(wn::producer, "TP");
-
-        Thread[] consumers = { //
-                new Thread(wn::consumer, "TC1"), //
-                new Thread(wn::consumer, "TC2"), //
-                new Thread(wn::consumer, "TC3") //
-        };
-
+        Thread producer = new Thread(wn::producer);
         producer.start();
+
+        Thread[] consumers = { new Thread(wn::consumer, "TC1"), new Thread(wn::consumer, "TC2"),
+                new Thread(wn::consumer, "TC3") };
         for (Thread consumer : consumers) {
             consumer.start();
         }
-
-        wn.checkThreadStates();
 
         for (Thread consumer : consumers) {
             consumer.join();
