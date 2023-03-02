e5d4d606a0bb704449825759796b58e4a4cb4c1b
Emanuele <egalli64@gmail.com>
2023-03-02 08:50:57 +0100

    more comments

--- src/main/java/com/example/jath/m1/s08/SynchroOnObjects.java
+++ src/main/java/com/example/jath/m1/s08/SynchroOnObjects.java
@@ -17,10 +17,11 @@ public class SynchroOnObjects {
     private int resourceB = 0;
 
     /**
-     * Six working threads divided in three groups
+     * Eight working threads divided in four groups
      * <li>Synchronized on this (standard) for System.out
      * <li>Synchronized on lock "A" for resourceA
      * <li>Synchronized on lock "B" for resourceB
+     * <li>Synchronized on lock "A" and then "B" for both resources
      * 
      * @param args not used
      */
@@ -28,12 +29,10 @@ public class SynchroOnObjects {
         SynchroOnObjects soo = new SynchroOnObjects();
 
         Thread[] threads = { //
-                new Thread(soo::syncOnThis, "This1"), //
-                new Thread(soo::syncOnA, "A1"), //
-                new Thread(soo::syncOnB, "B1"), //
-                new Thread(soo::syncOnThis, "This2"), //
-                new Thread(soo::syncOnA, "A2"), //
-                new Thread(soo::syncOnB, "B2") //
+                new Thread(soo::syncThis, "This1"), new Thread(soo::syncA, "A1"), //
+                new Thread(soo::syncB, "B1"), new Thread(soo::syncAB, "AB1"), //
+                new Thread(soo::syncThis, "This2"), new Thread(soo::syncA, "A2"), //
+                new Thread(soo::syncB, "B2"), new Thread(soo::syncAB, "AB2") //
         };
 
         for (Thread t : threads) {
@@ -50,25 +49,54 @@ public class SynchroOnObjects {
         System.out.println("Bye from " + Thread.currentThread().getName());
     }
 
-    public synchronized void syncOnThis() {
+
+    /**
+     * Synchronization for System.out
+     * 
+     * A and B are in read-only access, then there is no race condition on them
+     */
+    public synchronized void syncThis() {
         System.out.println(Thread.currentThread().getName() + " enter syncOnThis()");
         System.out.printf("%s A is %d, B is %d%n", Thread.currentThread().getName(), resourceA, resourceB);
         System.out.println(Thread.currentThread().getName() + " exit syncOnThis()");
     }
 
-    public void syncOnA() {
+    /**
+     * Synchronization for resourceA
+     */
+    public void syncA() {
+        System.err.println(Thread.currentThread().getName() + " enter sync block on A");
         synchronized (lockA) {
-            System.err.println(Thread.currentThread().getName() + " enter sync block on A");
             resourceA += 1;
-            System.err.println(Thread.currentThread().getName() + " exit sync block on A");
         }
+        System.err.println(Thread.currentThread().getName() + " exit sync block on A");
     }
 
-    public void syncOnB() {
+    /**
+     * Synchronization for resourceB
+     */
+    public void syncB() {
+        System.err.println(Thread.currentThread().getName() + " enter sync block on B");
         synchronized (lockB) {
-            System.err.println(Thread.currentThread().getName() + " enter sync block on G");
             resourceB += 1;
-            System.err.println(Thread.currentThread().getName() + " exit sync block on G");
         }
+        System.err.println(Thread.currentThread().getName() + " exit sync block on B");
+    }
+
+    /**
+     * Synchronization for A and then B
+     */
+    public void syncAB() {
+        System.err.println(Thread.currentThread().getName() + " enter sync block on A");
+        synchronized (lockB) {
+            resourceA *= 2;
+        }
+        System.err.println(Thread.currentThread().getName() + " exit sync block on A");
+
+        System.err.println(Thread.currentThread().getName() + " enter sync block on B");
+        synchronized (lockB) {
+            resourceB *= 2;
+        }
+        System.err.println(Thread.currentThread().getName() + " exit sync block on B");
     }
 }
