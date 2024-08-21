0e38f43011cb579c55121de0c1c63978df453547
Emanuele <egalli64@gmail.com>
2024-08-21 12:27:10 +0200

    minor review

--- src/main/java/com/example/jaco/m1/s5/MainForRunnable.java
+++ src/main/java/com/example/jaco/m1/s5/MainForRunnable.java
@@ -30,16 +30,18 @@ public class MainForRunnable {
 
         // Inject a runnable in a thread
         Thread worker = new Thread(new MyRunnable());
-        System.out.printf("%s is %s%n", worker.getName(), worker.getState());
+        // The thread state is NEW
+        System.out.println("After creation, the worker thread state is " + worker.getState());
 
         worker.start();
-        System.out.printf("%s is %s%n", worker.getName(), worker.getState());
+        // The thread state should be RUNNABLE
+        System.out.println("After starting, the worker thread state is " + worker.getState());
 
         // Doing something else in the main thread
         FakeTask.takeTime(300);
 
-        // Now the worker _should_ be TERMINATED
-        System.out.printf("%s is %s%n", worker.getName(), worker.getState());
+        // At this point the worker state should be TERMINATED
+        System.out.println("The worker thread state is now " + worker.getState());
         log.trace("Exit");
     }
 }
