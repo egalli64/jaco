ad47eb317e02fc08928fdd55406bda52f191a990
Emanuele <egalli64@gmail.com>
2024-08-25 12:15:48 +0200

    more material

---
+++ src/main/java/com/example/jaco/m5/s5/CFutureBasicIntro.java
@@ -0,0 +1,56 @@
+/*
+ * Introduction to Java Concurrency
+ * 
+ * https://github.com/egalli64/jaco
+ */
+package com.example.jaco.m5.s5;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * CompletableFuture introduction.
+ * <p>
+ * Even though it is possible to work on a completable future just in a single
+ * thread, its common use is about simplifying communication between threads.
+ */
+public class CFutureBasicIntro {
+    private static final Logger log = LoggerFactory.getLogger(CFutureBasicIntro.class);
+
+    /**
+     * Create a CompletableFuture and uses some of its most basic methods
+     * 
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        log.trace("Enter");
+        CompletableFuture<String> cf = new CompletableFuture<>();
+
+        if (!cf.isDone()) {
+            System.out.println("cf is not done yet");
+        } else {
+            System.out.println("Unexpected!");
+        }
+
+        log.debug("Completing cf");
+        cf.complete("Completed in the main thread");
+
+        if (cf.isDone()) {
+            System.out.println("cf has been completed");
+        } else {
+            System.out.println("Unexpected!");
+        }
+
+        try {
+            System.out.println("cf value by get: " + cf.get());
+        } catch (InterruptedException | ExecutionException e) {
+            log.warn("You should not get here", e);
+        }
+
+        System.out.println("cf value by join: " + cf.join());
+        log.trace("Exit");
+    }
+}
---
+++ src/main/java/com/example/jaco/m5/s5/ThenRun.java
@@ -0,0 +1,42 @@
+/*
+ * Introduction to Java Concurrency
+ * 
+ * https://github.com/egalli64/jaco
+ */
+package com.example.jaco.m5.s5;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.example.jaco.m1.s3.FakeTask;
+
+/**
+ * CompletableFuture::thenRun()
+ * <p>
+ * When the previous stage is done, a runnable is run
+ */
+public class ThenRun {
+    private static final Logger log = LoggerFactory.getLogger(ThenRun.class);
+
+    /**
+     * Create a completable future, print a message when done, do something else in
+     * the meantime
+     * 
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        log.trace("Enter");
+        CompletableFuture<Void> cf = CompletableFuture.supplyAsync(() -> FakeTask.adder(100)) //
+                .thenRun(() -> log.trace("In the runner"));
+
+        log.trace("Do something else until the future is not completed");
+        while (!cf.isDone()) {
+            System.out.printf("Main thread result: %f\n", FakeTask.adder(10));
+        }
+
+        System.out.println("The CompletableFuture returned by thenRun() is Void: " + cf.join());
+        log.trace("Exit");
+    }
+}
