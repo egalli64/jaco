9d3137fddff1c046bd6369aab2cc1e5b1b3c0070
Emanuele <egalli64@gmail.com>
2024-08-30 22:02:10 +0200

    moved and reviewed

---
+++ src/main/java/com/example/jaco/m6/s3/Complete1Plain.java
@@ -0,0 +1,45 @@
+/*
+ * Introduction to Java Concurrency
+ * 
+ * https://github.com/egalli64/jaco
+ */
+package com.example.jaco.m6.s3;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Synchronous completion by complete()
+ */
+public class Complete1Plain {
+    private static final Logger log = LoggerFactory.getLogger(Complete1Plain.class);
+
+    public static void main(String[] args) {
+        log.trace("Enter");
+
+        CompletableFuture<String> cf = new CompletableFuture<>();
+        log.debug("Created {}", cf);
+
+        cf.complete("Completed!");
+
+        if (cf.isDone()) {
+            log.debug("After completing");
+        } else {
+            log.warn("Unexpected!");
+        }
+
+        try {
+            log.info("Get: {}", cf.get());
+        } catch (InterruptedException ex) {
+            log.warn("Unexpected", ex);
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException ex) {
+            log.error("Nobody should throw anything here", ex);
+        }
+
+        log.info("Join: {}", cf.join());
+    }
+}
