8ab57603cfaa632f5f47f81667a937ff650e7755
Emanuele <egalli64@gmail.com>
2024-08-31 16:33:23 +0200

    split, move, and review

--- src/main/java/com/example/jaco/m6/s4/ThenAccept.java
+++ src/main/java/com/example/jaco/m6/s4/ThenAccept.java
@@ -3,7 +3,7 @@
  * 
  * https://github.com/egalli64/jaco
  */
-package com.example.jaco.m5.s7;
+package com.example.jaco.m6.s4;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -15,8 +15,8 @@ import com.example.jaco.m1.s3.FakeTask;
 /**
  * CompletableFuture::thenAccept()
  * <p>
- * The previous stage result is passed to a consumer, the resulting Future
- * contains a Void
+ * The previous stage result is passed to a consumer, the resulting
+ * CompletableFuture is Void
  */
 public class ThenAccept {
     private static final Logger log = LoggerFactory.getLogger(ThenAccept.class);
@@ -30,14 +30,13 @@ public class ThenAccept {
     public static void main(String[] args) {
         log.trace("Enter");
         CompletableFuture<Void> cf = CompletableFuture.supplyAsync(() -> FakeTask.adder(2)) //
-                .thenAccept(x -> System.out.printf("Worker: %f, %f\n", x, FakeTask.adder(10)));
+                .thenAccept(x -> log.info("Worker: {}, {}", x, FakeTask.adder(10)));
 
-        log.trace("Do something else until the future is not completed");
+        log.info("Do something else until the future is not completed");
         while (!cf.isDone()) {
-            System.out.printf("Main: %f\n", FakeTask.adder(2));
+            log.info("Main: {}", FakeTask.adder(2));
         }
 
-        System.out.println("Joining on a Void CompletableFuture gives ... " + cf.join());
-        log.trace("Exit");
+        log.info("Joining on a Void CompletableFuture gives ... {}", cf.join());
     }
 }
