47cd199011e2112a08f6f61b2409afcbb8d7909c
Emanuele <egalli64@gmail.com>
2025-03-03 16:18:48 +0100

    robustness

--- src/main/java/com/example/jaco/m1/s4/Main.java
+++ src/main/java/com/example/jaco/m1/s4/Main.java
@@ -26,6 +26,7 @@ public class Main {
             Thread.sleep(delta);
         } catch (InterruptedException e) {
             log.error("Sleep interrupted", e);
+            Thread.currentThread().interrupt();
         }
 
         System.out.printf("...sleep ended @ %d%n", System.currentTimeMillis());
