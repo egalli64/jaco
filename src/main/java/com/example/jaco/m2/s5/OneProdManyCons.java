c33b7c40b6144db26b875e4b580f960539a6341d
Emanuele <egalli64@gmail.com>
2025-03-05 18:19:40 +0100

    fix log name

--- src/main/java/com/example/jaco/m2/s5/OneProdManyCons.java
+++ src/main/java/com/example/jaco/m2/s5/OneProdManyCons.java
@@ -18,7 +18,7 @@ import org.slf4j.LoggerFactory;
  * terminates
  */
 public class OneProdManyCons {
-    private static final Logger log = LoggerFactory.getLogger(OneProdCon.class);
+    private static final Logger log = LoggerFactory.getLogger(OneProdManyCons.class);
 
     /** The resource shared between threads */
     private Product product = new Product();
