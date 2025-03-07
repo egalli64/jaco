ad6d9b0c35b55b02b473302bd242c8f25e44a7fe
Emanuele <egalli64@gmail.com>
2025-03-07 11:12:06 +0100

    tryAcquire

---
+++ src/main/java/com/example/jaco/m4/s2/SemaphoreTryAcquire.java
@@ -0,0 +1,42 @@
+/*
+ * Introduction to Java Concurrency
+ * 
+ * https://github.com/egalli64/jaco
+ */
+package com.example.jaco.m4.s2;
+
+import java.util.concurrent.Semaphore;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.example.jaco.m1.s3.FakeTask;
+
+/**
+ * A semaphore that allows PERMIT_NR threads at a time to access a critical area
+ */
+public class SemaphoreTryAcquire {
+    private static final Logger log = LoggerFactory.getLogger(SemaphoreTryAcquire.class);
+    private static final int PERMIT_NR = 2;
+    private static final int TASK_NR = 5;
+
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(PERMIT_NR);
+
+        Runnable worker = () -> {
+            if (semaphore.tryAcquire()) {
+                // no more than PERMIT_NR tasks could be running in this critical area
+                log.trace("Currently available permits: {}", semaphore.availablePermits());
+
+                FakeTask.takeTime(500);
+                semaphore.release();
+            } else {
+                log.trace("No permit available");
+            }
+        };
+
+        Stream.generate(() -> new Thread(worker)).limit(TASK_NR).forEach(Thread::start);
+        log.trace("Exit, leave the children threads to do the work");
+    }
+}
