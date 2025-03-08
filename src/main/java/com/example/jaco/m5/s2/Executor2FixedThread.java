0cf65ce0a76bd5b01d07045629081377c570e33c
Emanuele <egalli64@gmail.com>
2025-03-08 11:52:04 +0100

    review

--- src/main/java/com/example/jaco/m5/s2/Executor2FixedThread.java
+++ src/main/java/com/example/jaco/m5/s2/Executor2FixedThread.java
@@ -8,7 +8,6 @@ package com.example.jaco.m5.s2;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.DoubleAdder;
-import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +15,7 @@ import org.slf4j.LoggerFactory;
 import com.example.jaco.m1.s3.FakeTask;
 
 /**
- * Executor, ThreadPoolExecutor via Executors::newFixedThreadPool()
+ * Executor, ThreadPoolExecutor via Executors.newFixedThreadPool()
  */
 public class Executor2FixedThread {
     private static final Logger log = LoggerFactory.getLogger(Executor2FixedThread.class);
@@ -42,7 +41,9 @@ public class Executor2FixedThread {
 
         final long start = System.currentTimeMillis();
         try (ExecutorService executor = Executors.newFixedThreadPool(poolSize)) {
-            Stream.generate(() -> new Thread(task)).limit(TASK_NR).forEach(executor::execute);
+            for (int i = 0; i < TASK_NR; i++) {
+                executor.execute(task);
+            }
         }
         System.out.printf("Result is %.2f (%d ms)\n", result.sum(), System.currentTimeMillis() - start);
 
