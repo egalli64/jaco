a53ebd72d57d1c3dd56d70167d536d50ed065633
Emanuele <egalli64@gmail.com>
2025-03-10 12:32:05 +0100

    review

--- src/main/java/com/example/jaco/m6/s2/FutureTask2Callable.java
+++ src/main/java/com/example/jaco/m6/s2/FutureTask2Callable.java
@@ -3,7 +3,7 @@
  * 
  * https://github.com/egalli64/jaco
  */
-package com.example.jaco.m6.x1;
+package com.example.jaco.m6.s2;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
@@ -14,38 +14,47 @@ import org.slf4j.LoggerFactory;
 import com.example.jaco.m1.s3.FakeTask;
 
 /**
- * Running a FutureTask that returns a Double
+ * Running a FutureTask with a Callable.
+ * <p>
+ * Basic asynchronous execution and result retrieval
  */
 public class FutureTask2Callable {
     private static final Logger log = LoggerFactory.getLogger(FutureTask2Callable.class);
 
     /**
-     * Create a future task, run it in another thread, until is not done do
-     * something else, then print its result and terminate.
+     * Create a FutureTask with a Callable, start it in another thread, perform
+     * other tasks in the main thread, and then retrieve the result
      * 
      * @param args not used
      */
     public static void main(String[] args) throws Exception {
         log.trace("Enter");
 
+        // a FutureTask with a Callable, notice that it returns a value directly
         FutureTask<Double> task = new FutureTask<>(() -> {
             log.trace("Future task started");
-            return FakeTask.adder(50_000);
+            return FakeTask.adder(40_000);
         });
+
+        // start the FutureTask in a new thread
         new Thread(task).start();
 
         log.trace("While calculating the future task, do something in the main thread");
+        double result = 0.0;
+
         while (!task.isDone()) {
-            FakeTask.adder(5);
+            result += FakeTask.adder(4);
         }
+        System.out.println("Result in the main thread is " + result);
 
         try {
+            // block until the task is completed, then get its result
             System.out.println("Task result is " + task.get());
         } catch (InterruptedException ex) {
-            log.warn("Unexpected", ex);
+            log.warn("Task interrupted unexpectedly", ex);
             Thread.currentThread().interrupt();
         } catch (ExecutionException ex) {
-            log.error("Error in task execution", ex);
+            log.error("Unexpected error in task execution", ex);
         }
 
         log.trace("Exit");
