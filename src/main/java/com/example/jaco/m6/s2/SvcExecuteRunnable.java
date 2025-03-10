d0f73e88cb6613f49754e056fd25d9e87375c13a
Emanuele <egalli64@gmail.com>
2025-03-10 17:44:24 +0100

    clarify

---
+++ src/main/java/com/example/jaco/m6/s2/SvcExecuteRunnable.java
@@ -0,0 +1,48 @@
+/*
+ * Introduction to Java Concurrency
+ * 
+ * https://github.com/egalli64/jaco
+ */
+package com.example.jaco.m6.s2;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.example.jaco.m1.s3.FakeTask;
+
+/**
+ * Execute a Runnable on an Executor: fire and forget
+ * <p>
+ * No Future involved
+ */
+public class SvcExecuteRunnable {
+    private static final Logger log = LoggerFactory.getLogger(SvcExecuteRunnable.class);
+
+    /**
+     * Asynchronous execution with no Future involved
+     * 
+     * @param args not used
+     */
+    public static void main(String[] args) throws Exception {
+        log.trace("Enter");
+
+        Runnable task = () -> {
+            log.trace("Runnable started");
+            FakeTask.adder(40_000);
+        };
+
+        try (ExecutorService executor = Executors.newSingleThreadExecutor()) {
+            executor.execute(task);
+
+            log.trace("Main thread continues execution...");
+            for (int i = 0; i < 6; i++) {
+                FakeTask.adder(1_000);
+            }
+        }
+
+        log.trace("Exit");
+    }
+}
