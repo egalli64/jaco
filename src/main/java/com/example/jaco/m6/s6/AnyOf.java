514fec918bb2d76def12f25eccd23292c9429703
Emanuele <egalli64@gmail.com>
2025-03-12 17:56:45 +0100

    review

---
+++ src/main/java/com/example/jaco/m6/s6/AnyOf.java
@@ -0,0 +1,45 @@
+/*
+ * Introduction to Java Concurrency
+ * 
+ * https://github.com/egalli64/jaco
+ */
+package com.example.jaco.m6.s6;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.example.jaco.m1.s3.FakeTask;
+
+/**
+ * CompletableFuture::anyOf()
+ */
+public class AnyOf {
+    private static final Logger log = LoggerFactory.getLogger(AnyOf.class);
+
+    /**
+     * Create a few asynchronous CompletableFutures, create a new CompletableFuture
+     * that completes when all of them are completed. Do something else in main
+     * until it is done. Join the result.
+     * 
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        log.trace("Enter");
+
+        Supplier<Double> task = () -> FakeTask.adder(10);
+
+        CompletableFuture<Double> cf1 = CompletableFuture.supplyAsync(task);
+        CompletableFuture<Double> cf2 = CompletableFuture.supplyAsync(task);
+        CompletableFuture<Double> cf3 = CompletableFuture.supplyAsync(task);
+
+        CompletableFuture<Object> any = CompletableFuture.anyOf(cf1, cf2, cf3);
+        while (!any.isDone()) {
+            FakeTask.adder(1);
+        }
+
+        log.info("The first available CompletableFuture result is {}", any.join());
+    }
+}
