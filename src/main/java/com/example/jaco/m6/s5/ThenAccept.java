1d6c27649749ff12f54c37b22444a228eb636a3b
Emanuele <egalli64@gmail.com>
2025-03-12 11:29:40 +0100

    review

--- src/main/java/com/example/jaco/m6/s5/ThenAccept.java
+++ src/main/java/com/example/jaco/m6/s5/ThenAccept.java
@@ -3,9 +3,11 @@
  * 
  * https://github.com/egalli64/jaco
  */
-package com.example.jaco.m6.s4;
+package com.example.jaco.m6.s5;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,21 +24,29 @@ public class ThenAccept {
     private static final Logger log = LoggerFactory.getLogger(ThenAccept.class);
 
     /**
-     * Create a future, consumes to its result, do something else while the future
-     * is not done
+     * Create a future, consume to its result, do something else while the future is
+     * not done
      * 
      * @param args not used
      */
     public static void main(String[] args) {
         log.trace("Enter");
-        CompletableFuture<Void> cf = CompletableFuture.supplyAsync(() -> FakeTask.adder(2)) //
-                .thenAccept(x -> log.info("Worker: {}, {}", x, FakeTask.adder(10)));
+
+        Supplier<Double> task1 = () -> FakeTask.adder(2);
+
+        Consumer<Double> task2 = x -> {
+            log.info("Task 2 received {} from task 1", x);
+            double processedValue = x * FakeTask.adder(10);
+            log.info("Task 2 uses it input to generate {}", processedValue);
+        };
+
+        CompletableFuture<Void> cf = CompletableFuture.supplyAsync(task1).thenAccept(task2);
 
         log.info("Do something else until the future is not completed");
         while (!cf.isDone()) {
             FakeTask.adder(2);
         }
 
-        log.info("Joining on a Void CompletableFuture gives ... {}", cf.join());
+        log.info("Joining on a Void CompletableFuture gives ... {}", cf.join()); // null
     }
 }
