9c36789dbdfaf1392a1111aebfd2188c7af4615d
Emanuele <egalli64@gmail.com>
2025-03-13 22:44:24 +0100

    synchronized on list

---
+++ src/main/java/com/example/jaco/m7/s3/SynchronizedListAccess.java
@@ -0,0 +1,54 @@
+/*
+ * Introduction to Java Concurrency
+ * 
+ * https://github.com/egalli64/jaco
+ */
+package com.example.jaco.m7.s3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Access to a collection protecting the critical section with synchronized
+ */
+public class SynchronizedListAccess {
+    private static final Logger log = LoggerFactory.getLogger(SynchronizedListAccess.class);
+
+    private static final int LIST_SIZE = 10;
+    private static final int THREAD_NR = 5;
+    private static final int RUN_NR = 100_000;
+
+    public static void main(String[] args) throws InterruptedException {
+        log.trace("Enter");
+
+        List<Integer> list = new ArrayList<>(Collections.nCopies(LIST_SIZE, 0));
+
+        Runnable task = () -> {
+            for (int i = 0; i < RUN_NR; i++) {
+                // notice that in this example the list size is immutable
+                for (int j = 0; j < list.size(); j++) {
+                    // critical section
+                    synchronized (list) {
+                        list.set(j, list.get(j) + 1);
+                    }
+                }
+            }
+        };
+
+        try (ExecutorService executor = Executors.newFixedThreadPool(THREAD_NR)) {
+            for (int i = 0; i < THREAD_NR; i++) {
+                executor.execute(task);
+            }
+        }
+
+        int result = list.stream().mapToInt(Integer::intValue).sum();
+        System.out.printf("Expecting %d, result is %d\n", LIST_SIZE * THREAD_NR * RUN_NR, result);
+        log.trace("Exit");
+    }
+}
