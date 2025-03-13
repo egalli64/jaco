44cd1cf23b513b53fa6c092a7d5a3f7d28077b4f
Emanuele <egalli64@gmail.com>
2025-03-13 22:21:16 +0100

    review

--- src/main/java/com/example/jaco/m7/s2/UnsafeListAccess.java
+++ src/main/java/com/example/jaco/m7/s2/UnsafeListAccess.java
@@ -11,15 +11,22 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * "Normal" collection are not thread-safe
  */
 public class UnsafeListAccess {
+    private static final Logger log = LoggerFactory.getLogger(UnsafeListAccess.class);
+
     private static final int LIST_SIZE = 10;
     private static final int THREAD_NR = 5;
-    private static final int RUN_NR = 1_000;
+    private static final int RUN_NR = 100_000;
 
     public static void main(String[] args) throws InterruptedException {
+        log.trace("Enter");
+
         List<Integer> list = new ArrayList<>(Collections.nCopies(LIST_SIZE, 0));
 
         Runnable task = () -> {
@@ -39,5 +46,6 @@ public class UnsafeListAccess {
 
         int result = list.stream().mapToInt(Integer::intValue).sum();
         System.out.printf("Expecting %d, result is %d\n", LIST_SIZE * THREAD_NR * RUN_NR, result);
+        log.trace("Exit");
     }
 }
